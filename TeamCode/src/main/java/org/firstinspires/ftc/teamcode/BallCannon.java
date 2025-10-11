0a0561cb92fff124e8208c64c3bd1b01e0ec1eb2
Grey20wolf10 <grey20.wolf10@gmail.com>
2025-10-11 12:49:24 +0300

    изменил названия переменных, добавил методы rotate, inverse, stop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BallCannon.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BallCannon.java
@@ -3,24 +3,39 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class BallCannon {
     /* активировать моторы по кнопочке
-    */
-    DcMotor ShootingMotor;
+     */
+    DcMotor shootingMotor;
 
+    boolean switchMotor = false;
+    boolean protectionClampedButton = false;
 
+    boolean altButton = false;
 
-    public void BallCannon(LinearOpMode opMode) {
-        ShootingMotor = opMode.hardwareMap.get(DcMotor.class, "ShootingMotor");
+    //    public void setAltButton(&& altB){
+//        a
+//    }
+    public BallCannon(HardwareMap hard) {
+        shootingMotor = hard.get(DcMotor.class, "shootingMotor");
+        shootingMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
 
     }
-    public void Shoot(boolean g2b) {
-        if (g2b){
-            ShootingMotor.setPower(1);
+
+    public void rotate() {
+        shootingMotor.setPower(1);
     }
-        else{
-            ShootingMotor.setPower(0);
-        }
+
+    public void stop() {
+        shootingMotor.setPower(0);
     }
+
+    public void inverse() {
+        shootingMotor.setPower(-1);
+    }
+
+
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
@@ -2,14 +2,29 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+@com.qualcomm.robotcore.eventloop.opmode.TeleOp(name = "TeleOp", group = "Linear OpMode")
 public class TeleOp extends LinearOpMode {
     BallCannon ballCannon;
+    boolean stateButtonB;
+    int mode=0;
     @Override
     public void runOpMode() throws InterruptedException {
-        ballCannon = new BallCannon();
+        ballCannon = new BallCannon(hardwareMap);
         waitForStart();
-        while (opModeIsActive()){
-            ballCannon.Shoot(gamepad2.b);
+        while (opModeIsActive()) {
+            if (mode==1) {
+                ballCannon.rotate();
+            } else if (mode==0) {
+                ballCannon.stop();
+            } else {
+                ballCannon.inverse();
+            }
+            if(stateButtonB && !gamepad2.b){
+                mode+=1;
+                mode=mode%3;
+
+            }
+            stateButtonB = gamepad2.b;
 
         }
 
