001e4841c0822d69ceba93c9ba0efcfe9e8de084
amrogozov <am.rogozov@gmail.com>
2025-10-11 10:42:17 +0300

    Добавление комментариев и удаление не нужных импортов

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoRidePath.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoRidePath.java
@@ -2,30 +2,37 @@ package org.firstinspires.ftc.teamcode;
 
 import com.pedropathing.follower.Follower;
 import com.pedropathing.geometry.BezierLine;
-import com.pedropathing.geometry.Curve;
-import com.pedropathing.geometry.PedroCoordinates;
 import com.pedropathing.geometry.Pose;
-import com.pedropathing.paths.Path;
 import com.pedropathing.paths.PathBuilder;
-import com.pedropathing.paths.PathConstraints;
 import com.pedropathing.paths.PathChain;
+
 public class AutoRidePath {
-    private final Pose startPose = new Pose (24, 144, Math.toRadians(180));
-    private final Pose scorePose = new Pose (24, 120, Math.toRadians(45));
-    private final Pose middlePose = new Pose (48, 72, Math.toRadians(180));
-    private  final Pose firstColumnPose = new Pose (36,84, Math.toRadians(180));
-    private  final Pose secondColumnPose = new Pose (36,60, Math.toRadians(180));
-    private  final Pose thirdColumnPose = new Pose (36,36, Math.toRadians(180));
-    private  final Pose grabFirstPose = new Pose (12,84, Math.toRadians(180));
-    private  final Pose grabSecondPose = new Pose (12,60, Math.toRadians(180));
-    private  final Pose grabThirdPose = new Pose (12,36, Math.toRadians(180));
+    //позиция робота в начале матча
+    private final Pose startPose = new Pose(24, 144, Math.toRadians(180));
+    //позиция возле колодца для стрельбы
+    private final Pose scorePose = new Pose(24, 120, Math.toRadians(45));
+    //позиция в центре поля, чтобы не задевать мячи при поездки
+    private final Pose middlePose = new Pose(48, 72, Math.toRadians(180));
+    //позиция перед 1 колонной с мячами для последующего их подбора
+    private final Pose firstColumnPose = new Pose(36, 84, Math.toRadians(180));
+    //позиция перед 2 колонной с мячами для последующего их подбора
+    private final Pose secondColumnPose = new Pose(36, 60, Math.toRadians(180));
+    //позиция перед 3 колонной с мячами для последующего их подбора
+    private final Pose thirdColumnPose = new Pose(36, 36, Math.toRadians(180));
+    //позиция после 1 колонной с мячами для подбора мячей при поездки
+    private final Pose grabFirstPose = new Pose(12, 84, Math.toRadians(180));
+    //позиция после 2 колонной с мячами для подбора мячей при поездки
+    private final Pose grabSecondPose = new Pose(12, 60, Math.toRadians(180));
+    //позиция после 3 колонной с мячами для подбора мячей при поездки
+    private final Pose grabThirdPose = new Pose(12, 36, Math.toRadians(180));
     private Follower follower;
     private PathChain firstScore, fromScoreToFirstColumn, fromScoreToSecondColumn, fromScoreToThirdColumn, toScore;
     PathBuilder path = follower.pathBuilder()
 
 
             .addPath(new BezierLine(startPose, scorePose))
-            .setLinearHeadingInterpolation(startPose.getHeading(), scorePose.getHeading()
+            .setLinearHeadingInterpolation(startPose.getHeading(), scorePose.getHeading());
+
     public void pathBuild() {
         firstScore = follower.pathBuilder()
                 .addPath(new BezierLine(startPose, scorePose))
@@ -62,7 +69,7 @@ public class AutoRidePath {
                 .addPath(new BezierLine(middlePose, scorePose))
                 .setLinearHeadingInterpolation(middlePose.getHeading(), scorePose.getHeading())
                 .build();
-        
+
     }
 
     public void AutoFollowPath() {
