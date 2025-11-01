7c56581628c79bc112f9d6f025dbb812f66ab674
am.rogozov <am.rogozov@gmail.com>
2025-11-01 12:56:13 +0300

    добавили в TeleOp управление мотором для подачи шариков при выстреле и закоментили несколько строк в gradle

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
@@ -1,24 +1,34 @@
 package org.firstinspires.ftc.teamcode;
-import com.acmerobotics.dashboard.FtcDashboard;
-import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+//import com.acmerobotics.dashboard.FtcDashboard;
+//import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import org.firstinspires.ftc.teamcode.modules.MechTrain;
 
 @com.qualcomm.robotcore.eventloop.opmode.TeleOp(name = "TeleOp", group = "Linear OpMode")
 public class TeleOp extends LinearOpMode {
-    MechTrain mechTrain;
+//    MechTrain mechTrain;
 
     BallCannon ballCannon;
 
     @Override
     public void runOpMode() throws InterruptedException {
         ballCannon = new BallCannon(hardwareMap);
-        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+//        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
 
-        mechTrain = new MechTrain(this);
+//        mechTrain = new MechTrain(this);
         waitForStart();
         while (opModeIsActive()) {
-            mechTrain.setPowerOnMecanumBase(gamepad1.left_stick_x, gamepad1.left_stick_y, gamepad1.left_trigger - gamepad1.right_trigger);
+//            mechTrain.setPowerOnMecanumBase(gamepad1.left_stick_x, gamepad1.left_stick_y, gamepad1.left_trigger - gamepad1.right_trigger);
+            ballCannon.setPower(-gamepad2.right_stick_y);
+            ballCannon.controlShootingMotor(gamepad2.b);
+            telemetry.addData("Mode", ballCannon.mode);
+            telemetry.addData("Velosity", ballCannon.velosityMotor());
+            // 1. выведи все переменные в консол
+            // 2. выведи в консоль управлять
+            telemetry.addLine("b-1нажатие вращение мотора по часовой;b-повторное нажатие вращение мотора против часовой;b-третье нажатие остановка мотора");
+            telemetry.update();
+            ballCannon.controlBallPushingMotor(gamepad2.x);
+
         }
     }
 }
