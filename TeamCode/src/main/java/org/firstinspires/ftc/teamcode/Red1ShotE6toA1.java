94359fa6abb772aa3004f733d2af6d5f6cc21a4f
am.rogozov <am.rogozov@gmail.com>
2025-11-01 12:52:31 +0300

    автономы сережи и паши для синего и крастного альянсов

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Red1ShotE6toA1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Red1ShotE6toA1.java
@@ -2,22 +2,41 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.modules.MechTrain;
 
 @Autonomous
 
 public class Red1ShotE6toA1 extends LinearOpMode {
-
+    private ElapsedTime timer = new ElapsedTime();
     public void runOpMode() {
-        MechTrain mechTrain = new MechTrain(this);
         waitForStart();
-        // стреляем
-        //mechTrain.moveBackRight(1,Math.sqrt(2)*48);
-        mechTrain.moveBack(1, 48);
-        mechTrain.moveRight(1, 120);
-        mechTrain.moveBack(1, 48);
-
+        BallCannon ballCannon = new BallCannon(hardwareMap);
+        double motorPower = 1;
+        if (opModeIsActive()) {
+            // стреляем
+            ballCannon.inverse(motorPower);
+            double durationForPushingBallNs = 3.5e9;
+            double durationForShootingBallNs = 3e9;
+            double durationForPushingBalls = 1e9;
+            timer.reset();
+            ballCannon.controlShootingMotor(true);
+            ballCannon.controlShootingMotor(true);
+            while (!(durationForShootingBallNs <timer.nanoseconds())) {
+            }
+            ballCannon.controlBallPushingMotor(true);
+            while (!(durationForPushingBallNs < timer.nanoseconds())) {
+            }
+            ballCannon.controlBallPushingMotor(false);
+            ballCannon.controlShootingMotor(true);
+            //mechTrain.moveBackRight(1,Math.sqrt(2)*48);
+            MechTrain mechTrain = new MechTrain(this);
+            mechTrain.moveBack(1, 48);
+            mechTrain.moveRight(1, 120);
+            mechTrain.moveBack(1, 48);
+        }
     }
 
 }
