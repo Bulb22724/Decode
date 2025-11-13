e983b3644023bed5f2e1984d8cad9fa4e7be6253
Grey20wolf10 <grey20.wolf10@gmail.com>
2025-11-13 19:26:27 +0300

    подписал функции управления моторами в автономе

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoShoot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoShoot.java
@@ -15,31 +15,33 @@ public class AutoShoot extends LinearOpMode {
         waitForStart();
         BallCannon ballCannon = new BallCannon(this);
         MechTrain mechTrain = new MechTrain(this);
-            // стреляем
-            
-            double shootingTime = 3;
-            double pushingTime = 1;
+        // стреляем
 
+        double shootingTime = 3;
+        double pushingTime = 1;
 
-            mechTrain.moveBack(0.5,18);
-            
-            ballCannon.rotateShootingMotor();
-            timer.reset();
-            while (shootingTime > timer.seconds() && opModeIsActive()) {
-            }
-            ballCannon.rotatePushingMotor();
+        //отезжаем назад для занимания огневой позиции
+        mechTrain.moveBack(0.5, 18);
+        //раскручивам колесо для стрельбы
+        ballCannon.rotateShootingMotor();
+        timer.reset();
+        while (shootingTime > timer.seconds() && opModeIsActive()) {
+        }
+        //раскручивам колесо для подачи шариков
+        ballCannon.rotatePushingMotor();
 
-            timer.reset();
-            while (pushingTime > timer.seconds() && opModeIsActive()) {
-            }
-            ballCannon.stopShootingMotor();
-            ballCannon.stopPushingMotor();
-            //mechTrain.moveBackRight(1,Math.sqrt(2)*48);
+        timer.reset();
+        while (pushingTime > timer.seconds() && opModeIsActive()) {
+        }
+        //останавливаем оба колеса
+        ballCannon.stopShootingMotor();
+        ballCannon.stopPushingMotor();
+        //mechTrain.moveBackRight(1,Math.sqrt(2)*48);
 //            MechTrain mechTrain = new MechTrain(this);
 //            mechTrain.moveBack(1, 48);
 //            mechTrain.moveLeft(1, 120);
 //            mechTrain.moveBack(1, 48);
-        }
+    }
 
 }
 
