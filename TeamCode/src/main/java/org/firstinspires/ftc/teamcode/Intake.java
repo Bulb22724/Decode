caf0df0b3fa08c25d34bac91718b3a9aaf694228
Grey20wolf10 <grey20.wolf10@gmail.com>
2025-11-29 11:05:30 +0300

    исправил ошибки и добавил телеметрию

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Intake.java
@@ -1,88 +1,80 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+@Config
 public class Intake {
     DcMotorEx intakeMotor;
-    boolean oldStateg1a;
-    boolean oldStateg1b;
-    boolean oldStateg1x;
-    boolean oldStateg1y;
-    /**
-     * stateIntake хранит состояние intakeMotor 1-включен,0-выключен
-     */
-    int stateIntake=0;
-    /**
-     * хранит направление вращения intakeMotor 1-вперед,2-назад
-     */
-    int directionIntake=1;
-    /**
-     * moduleIntakeMotorPower хранит модуль мощность intakeMotor
-     */
-    double moduleIntakeMotorPower=0.5;
-    /**
-     * IntakeMotorPower хранит мощность intakeMotor
-     */
-    double intakeMotorPower=moduleIntakeMotorPower;
+    LinearOpMode opMode;
+    // isOn хранит состояние intakeMotor работает.не работает
+    boolean isOn = false;
+    // хранит направление вращения intakeMotor
+    boolean isIn = true;
+    // absMotorPower хранит модуль мощности intakeMotor
+    public static double absMotorPower = 0.5;
+
     public Intake(LinearOpMode opMode) {
         intakeMotor = opMode.hardwareMap.get(DcMotorEx.class, "intakeMotor");
         intakeMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        this.opMode = opMode;
     }
 
     /**
-     * intakeOn включает intakeMotor с мощьностью intakeMotorPover при переходе кнопки а 1 джостика из состояния выключенно в состояние включенно
-     * @param g1a принимет значение кнопки а первого джостика
-     * oldStateg1a преведущее значение состояния кнопки а первого джостика
+     * on включает intakeMotor с мощностью absMotorPower
      */
-    public void intakeOn(boolean g1a){
-        if(g1a && !oldStateg1a && stateIntake!=1){
-            stateIntake=1;
-            intakeMotor.setPower(intakeMotorPower);
+    public void on() {
+        if (!isOn) {
+            isOn = true;
+            intakeMotor.setPower(absMotorPower);
         }
-        oldStateg1a =g1a;
+
     }
+
     /**
-     * intakeOff выключает intakeMotor при переходе кнопки b 1 джостика из состояния выключенно в состояние включенно
-     * @param g1b принимет значение кнопки b первого джостика
-     * oldStateg1b преведущее значение состояния кнопки b первого джостика
+     * off выключает intakeMotor
      */
-    public void intakeOff(boolean g1b){
-        if(g1b && !oldStateg1b && stateIntake!=0){
-            stateIntake=0;
+    public void off() {
+        if (isOn) {
+            isOn = false;
             intakeMotor.setPower(0);
         }
-        oldStateg1b =g1b;
+
     }
+
     /**
-     * intakeIn изменяет направление вращения intakeMotor  при переходе кнопки x 1 джостика из состояния выключенно в состояние включенно
-     * @param g1x принимет значение кнопки x первого джостика
-     * oldStateg1x преведущее значение состояния кнопки x первого джостика
+     * in изменяет направление вращения intakeMotor
      */
-    public void intakeIn(boolean g1x){
-        if(g1x && !oldStateg1x && directionIntake!=1){
-            directionIntake=1;
-            intakeMotorPower=moduleIntakeMotorPower;
-            if(stateIntake==1){
-                intakeMotor.setPower(intakeMotorPower);
-            }
+    public void in() {
+        if (!isIn) {
+            isIn = true;
+            intakeMotor.setDirection(DcMotorSimple.Direction.FORWARD);
         }
-        oldStateg1x =g1x;
+
     }
+
     /**
-     * intakeOut изменяет направление вращения intakeMotor  при переходе кнопки y 1 джостика из состояния выключенно в состояние включенно
-     * @param g1y принимет значение кнопки y первого джостика
-     * oldStateg1y преведущее значение состояния кнопки y первого джостика
+     * out изменяет направление вращения intakeMotor
      */
-    public void intakeOut(boolean g1y){
-        if(g1y && !oldStateg1y && directionIntake!=2){
-            directionIntake=2;
-            intakeMotorPower=-moduleIntakeMotorPower;
-            if(stateIntake==1){
-                intakeMotor.setPower(intakeMotorPower);
-            }
+    public void out() {
+        if (isIn) {
+            isIn = false;
+            intakeMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         }
-        oldStateg1y =g1y;
+    }
+
+    public void addData() {
+        Telemetry telemetry = opMode.telemetry;
+        telemetry.addData("Направление захвата", intakeMotor.getDirection());
+        telemetry.addData("мотор включен", isOn);
+        telemetry.addData("мотор вращается внутрь", isIn);
+        telemetry.addData("модуль мощности мотора", absMotorPower);
+        telemetry.addData("скорость вращения мотора", intakeMotor.getVelocity());
     }
 }
